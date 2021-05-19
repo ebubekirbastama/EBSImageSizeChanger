dd7c67a44682af8947283eb4830759a36d49ae18
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-05-19 13:01:02 +0300

    Add files via upload

---
+++ EBSİmageChanger/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EBSİmageChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_Yukseklik = new System.Windows.Forms.TextBox();
+            this.txt_Genislik = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Resimİsmi = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Resimleri Değiştir.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_Yukseklik
+            // 
+            this.txt_Yukseklik.Location = new System.Drawing.Point(151, 44);
+            this.txt_Yukseklik.Name = "txt_Yukseklik";
+            this.txt_Yukseklik.Size = new System.Drawing.Size(100, 20);
+            this.txt_Yukseklik.TabIndex = 1;
+            // 
+            // txt_Genislik
+            // 
+            this.txt_Genislik.Location = new System.Drawing.Point(31, 44);
+            this.txt_Genislik.Name = "txt_Genislik";
+            this.txt_Genislik.Size = new System.Drawing.Size(100, 20);
+            this.txt_Genislik.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Genislik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Yükseklik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resim İsmi";
+            // 
+            // txt_Resimİsmi
+            // 
+            this.txt_Resimİsmi.Location = new System.Drawing.Point(85, 102);
+            this.txt_Resimİsmi.Name = "txt_Resimİsmi";
+            this.txt_Resimİsmi.Size = new System.Drawing.Size(100, 20);
+            this.txt_Resimİsmi.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(277, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(493, 186);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(779, 207);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_Resimİsmi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Genislik);
+            this.Controls.Add(this.txt_Yukseklik);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EBS Resim Boyutlandırıcı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_Yukseklik;
+        private System.Windows.Forms.TextBox txt_Genislik;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Resimİsmi;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ EBSİmageChanger/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EBSİmageChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread th; OpenFileDialog op;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            op.Multiselect = true;
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                th = new Thread(Changer); th.Start();
+            }
+        }
+
+        private void Changer()
+        {
+            if (listBox1.Items.Count != 0)
+            {
+                listBox1.Items.Clear();
+            }
+            else
+            {
+                for (int i = 0; i < op.FileNames.Length; i++)
+                {
+                    listBox1.Items.Add(op.FileNames[i].ToString());
+                }
+                for (int i = 0; i < op.FileNames.Length; i++)
+                {
+                    Image image = Image.FromFile(op.FileNames[i].ToString());
+                    string Yol = op.FileNames[i].ToString().Replace(op.SafeFileNames[i].ToString(), "");
+                    İmageSizeChanger(image, int.Parse(txt_Genislik.Text), int.Parse(txt_Yukseklik.Text), txt_Resimİsmi.Text);
+                }
+            }
+        }
+
+        public void İmageSizeChanger(Image img, int Genislik, int Yukseklik, string FolderOrName)
+        {
+            Bitmap Newİmage = new Bitmap(Genislik, Yukseklik);
+            using (Graphics g = Graphics.FromImage((Image)Newİmage))
+            {
+                g.DrawImage(img, 0, 0, Genislik, Yukseklik);
+            }
+            Newİmage.Save(FolderOrName, ImageFormat.Jpeg);
+        }
+    }
+}
